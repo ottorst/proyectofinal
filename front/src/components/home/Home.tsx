e87bbe02f123b39ca895f5d5cf64aceceb028b13
Andres Rodriguez <banrostin@gmail.com>
2024-07-20 12:06:56 -0600

    prueba 2 front git

--- front/src/components/home/Home.tsx
+++ front/src/components/home/Home.tsx
@@ -11,6 +11,7 @@ const Home:React.FC = () => {
         <div className="relative w-full md:w-[509px] md:h-[423px] flex items-center text-center font-serif text-[20px] md:text-[25px]">
           <p className="text-center px-4">Chef Mike is committed to using only the highest quality ingredients in his culinary creations. He meticulously sources fresh, organic produce, sustainably caught seafood, and ethically raised meats to ensure every dish meets the highest standards of flavor and nutrition. </p>
           <p>PRUEBA DE FRONT</p>
+          <p>Prueba 2</p>
         </div>
         <div className="relative flex-shrink-0 w-full md:w-auto">
           <Image src="/assets/tablacomida.png" alt="Tabla Carnes frias" width={500} height={450}/>
