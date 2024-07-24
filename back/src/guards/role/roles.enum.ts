1321686341ec0e10eb08b33571a2a55ed966e314
brianpadilla3072 <padillabrian830@gmail.com>
2024-07-23 23:48:21 -0300

    create role

---
+++ back/src/guards/role/roles.enum.ts
@@ -0,0 +1,4 @@
+export enum Rol {
+    User = 'user',
+    Admin = 'admin'
+}
--- back/src/modules/events/events.controller.ts
+++ back/src/modules/events/events.controller.ts
@@ -6,7 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards
 } from '@nestjs/common';
+import {AuthGuard} from "src/guards/auth/auth.guard" 
+import { Rol } from "src/guards/role/roles.enum";
+import {Roles} from "src/decorators/roles/rol.decorator"
+import {RolesGuards} from "src/guards/role/roles.guard";
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -21,7 +26,8 @@ export class EventsController {
   create(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.create(createEventDto);
   }
-
+  @Roles(Rol.Admin)
+  @UseGuards(AuthGuard,RolesGuards)
   @Get()
   findAll() {
     return this.eventsService.findAll();
