4809e98301832bbd6d564be0fd7db575f982a033
amandabrousson <amanda.brousson@gmail.com>
2024-07-25 15:51:19 -0300

    Auth Context

--- front/src/components/AuthContext.tsx
+++ front/src/components/AuthContext.tsx
@@ -90,7 +90,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 export const useAuth = (): AuthContextProps => {
     const context = useContext(AuthContext);
     if (!context) {
-        throw new Error('useAuth debe usarse dentro de un AuthProvider');
+        throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
 };
