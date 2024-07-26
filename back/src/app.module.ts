ccc4ddd2eec4685ced4a27bcf4a3f1cd03a82398
ottorst <otto.st@gmail.com>
2024-07-26 17:20:33 +0100

    Events and Booking Entities and DTO

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './modules/users/users.module';
 import { EventsModule } from './modules/events/events.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { BookingModule } from './modules/booking/booking.module';
 
 @Module({
-  imports: [UsersModule, EventsModule, AuthModule, PrismaModule],
+  imports: [UsersModule, EventsModule, AuthModule, PrismaModule, BookingModule],
   controllers: [AppController],
   providers: [AppService],
 })
