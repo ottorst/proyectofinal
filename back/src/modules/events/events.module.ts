d7bb22100b25331b53e67794aa9ccafc8dce748b
ottorst <147392981+ottorst@users.noreply.github.com>
2024-08-04 16:59:26 +0100

    Revert "create endpoint update image and swagger documntation"

--- back/src/modules/events/events.module.ts
+++ back/src/modules/events/events.module.ts
@@ -5,7 +5,6 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 
 
-
 @Module({
   controllers: [EventsController],
   providers: [EventsService,ConfigService,PrismaService],
