9b580daedcbea978b02f7968123f44e47893987f
brianpadilla3072 <padillabrian830@gmail.com>
2024-08-08 00:13:02 -0300

    crate resgister and booking template html  and endpoind

--- back/src/modules/send-email/sendEmail.module.ts
+++ back/src/modules/send-email/sendEmail.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SendEmailService } from './sendEmail.service';
 import { SendEmailController } from './sendEmail.controller';
 import { NodemailerService } from 'src/nodemailer/nodemailer.service';
+import { TemplateService } from './template.service';
 
 @Module({
-  providers: [SendEmailService,NodemailerService],
+  providers: [SendEmailService,NodemailerService, TemplateService],
   controllers: [SendEmailController],
 })
 export class SendEmailModule {}
